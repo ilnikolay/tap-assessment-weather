17585ff66e5de976ca4dc7a9e0125894334d4705
ilnikolay <94624457+ilnikolay@users.noreply.github.com>
2022-01-16 16:31:32 +0200

    Dev (#1)
    * Fixed apps.py, finished utils.py and partially implemented country_data_manager.py
    
    * edit readme
    
    * Editing Readme
    
    * Edit Readme
    
    * adding the CI yam for dev
    
    * saving current state of this file
    
    * fixing working with cities
    
    * The logic of the app is working correctly

--- country_data_manager.py
+++ country_data_manager.py
@@ -7,12 +7,27 @@ class CountryDataManager:
     def __init__(self, filename: str) -> None:
         self._data = load_countries_from_file(filename)
 
+    @staticmethod
+    def filt_func(filters, c_name: str):
+        if filters is None or not any(filters.values()):
+            return True
+        if all(filters.values()) and len(filters) == 2:
+            if filters['contains'].lower() in c_name.lower() and \
+                c_name.lower().startswith(filters['starts_with'].lower()):
+                return True
+        else:
+            try:
+                return bool(filters['contains'].lower() in c_name.lower())
+            except (KeyError, AttributeError):
+                return bool(c_name.lower().startswith(filters['starts_with'].lower()))
+        return False
+
     def list_countries(self, filters: Dict[str, Optional[str]] = None) -> List[str]:
-        # todo: implement getting countries from self._data with filtering, ignore case, see tests.py and README.md
-        #  for more details
-        pass
+        filtered_output = [country for country in self._data if self.filt_func(filters,country)]
+        return filtered_output
 
     def list_cities(self, country: str, filters: Dict[str, Optional[str]] = None) -> List[str]:
-        # todo: implement getting cities for a country from self._data with filtering, ignore case, see tests.py and
-        #  README.md for more details
-        pass
+        for country_name, cities in self._data.items():
+            if country_name == country:
+                city_list = [city for city in cities if self.filt_func(filters,city)]
+        return city_list
